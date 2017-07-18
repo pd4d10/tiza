9cfef9c86fd7a6830e5d7bb5de778d4a5ee46838
pd4d10 <pd4d10@gmail.com>
2017-07-18 13:37:33 +0800

    Add style stringify and log

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const colors = ['red', 'blue', 'green']
 
+// Stringify styles
+function stringify(obj) {
+  return Object.keys(obj)
+    .map(key => {
+      return `${key}:${obj[key]}`
+    })
+    .join(';')
+}
+
 function cholk(text) {
   return {
     style: cholk._style,
@@ -8,6 +17,20 @@ function cholk(text) {
 }
 
 cholk._style = {}
+cholk.log = (...args) => {
+  const results = []
+  const styles = []
+  args.forEach(arg => {
+    if (typeof arg === 'object' && arg.style) {
+      results.push(`%c${arg.text}`)
+      styles.push(stringify(arg.style))
+    } else {
+      results.push(`%c${arg}`)
+      styles.push('')
+    }
+  })
+  console.log(results.join(''), ...styles)
+}
 
 const proto = Object.create(null)
 const properties = colors.reduce((props, color) => {
@@ -15,7 +38,7 @@ const properties = colors.reduce((props, color) => {
     get() {
       cholk._style.color = color
       return cholk
-    }
+    },
   }
   return props
 }, {})
