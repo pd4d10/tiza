5f60cacd6ffd61713961069adc0d44389be86400
pd4d10 <pd4d10@gmail.com>
2017-07-21 11:44:57 +0800

    Refactor rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,21 @@
-// https://github.com/rollup/rollup/issues/703#issuecomment-306246339
-
-const version = require('./package.json').version;
+import babel from 'rollup-plugin-babel'
+import license from 'rollup-plugin-license'
+import { version } from './package.json'
+import uglify from 'rollup-plugin-uglify'
 
 const banner = `/**
  * Tiza v${version}
  * Copyright (c) 2017 pd4d10
  * Released under the MIT License.
  * https://github.com/pd4d10/tiza
- */`;
+ */`
 
+// https://github.com/rollup/rollup/issues/703#issuecomment-306246339
 export default [
   {
     entry: 'src/index.js',
     plugins: [
-      require('rollup-plugin-babel')({
+      babel({
         exclude: 'node_modules/**',
       }),
     ],
@@ -23,10 +25,10 @@ export default [
   {
     entry: 'src/index.js',
     plugins: [
-      require('rollup-plugin-babel')({
+      babel({
         exclude: 'node_modules/**',
       }),
-      require('rollup-plugin-license')({
+      license({
         banner,
       }),
     ],
@@ -37,13 +39,13 @@ export default [
   {
     entry: 'src/index.js',
     plugins: [
-      require('rollup-plugin-babel')({
+      babel({
         exclude: 'node_modules/**',
       }),
-      require('rollup-plugin-uglify')(),
+      uglify(),
     ],
     format: 'umd',
     moduleName: 'tiza',
     dest: 'dist/tiza.min.js',
   },
-];
+]
