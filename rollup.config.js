518e901f8bffbbbe270a27cb0900160c38208c45
Rongjian Zhang <pd4d10@gmail.com>
2018-06-17 17:41:39 +0800

    Use preset env instead of es2015

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,35 @@
 import babel from 'rollup-plugin-babel'
 import license from 'rollup-plugin-license'
-import { version } from './package.json'
 import uglify from 'rollup-plugin-uglify'
 
-const banner = `/**
- * Tiza v${version}
- * Copyright (c) 2017 pd4d10
+const babelPlugin = babel({ exclude: 'node_modules/**' })
+const licensePlugin = license({
+  banner: `/**
+ * Tiza v${require('./package.json').version}
+ * Copyright (c) 2017 Rongjian Zhang
  * Released under the MIT License.
  * https://github.com/pd4d10/tiza
- */`
+ */`,
+})
 
 // https://github.com/rollup/rollup/issues/703#issuecomment-306246339
 export default [
   {
     entry: 'src/index.js',
-    plugins: [
-      babel({
-        exclude: 'node_modules/**',
-      }),
-    ],
+    plugins: [babelPlugin],
     format: 'cjs',
     dest: 'dist/tiza.common.js',
   },
   {
     entry: 'src/index.js',
-    plugins: [
-      babel({
-        exclude: 'node_modules/**',
-      }),
-      license({
-        banner,
-      }),
-    ],
+    plugins: [babelPlugin, licensePlugin],
     format: 'umd',
     moduleName: 'tiza',
     dest: 'dist/tiza.js',
   },
   {
     entry: 'src/index.js',
-    plugins: [
-      babel({
-        exclude: 'node_modules/**',
-      }),
-      uglify(),
-      license({
-        banner,
-      }),
-    ],
+    plugins: [babelPlugin, uglify(), licensePlugin],
     format: 'umd',
     moduleName: 'tiza',
     dest: 'dist/tiza.min.js',
