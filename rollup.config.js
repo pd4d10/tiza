25f747075bed50b4c22baef35d5fdb1b8c13b70a
Rongjian Zhang <pd4d10@gmail.com>
2018-10-12 22:05:29 +0800

    build: output typescript declaration

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import typescript from 'rollup-plugin-typescript'
+import typescript from 'rollup-plugin-typescript2'
 import license from 'rollup-plugin-license'
 import { uglify } from 'rollup-plugin-uglify'
 
